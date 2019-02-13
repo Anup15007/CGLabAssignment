9fb0c100af2525d19baadb507628d7ba17bb79e7
PatitPawan <pp1197barik@gmail.com>
2019-02-14 00:31:00 +0530

    Add files via upload

--- LabPracticeDay7/src/com/cg/eis/client/MainClass.java
+++ LabPracticeDay7/src/com/cg/eis/client/MainClass.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
-
 import com.cg.eis.beans.Employee;
 import com.cg.eis.services.EmployeeService;
 import com.cg.eis.services.InsuranceServiceDemo;
@@ -14,12 +13,14 @@ public class MainClass
 	public static void main(String args[])
 	{	Scanner sc = new Scanner(System.in);
 		InsuranceServiceDemo services = new InsuranceServiceDemo();
-		Employee employ1 = new Employee("Prajjval",101,45000,"manager",services.findScheme(45000,"manager"));
-		Employee employ2 = new Employee("Girish",102,25000,"programmer",services.findScheme(25000,"programmer"));
-		Employee employ3 = new Employee("Dhingra",103,21000,"programmer",services.findScheme(21000,"programmer"));
+		Employee employ1 = new Employee("PATIT PAWAN",101,40000,"MANAGER",services.findScheme(45000,"MANAGER"));
+		Employee employ2 = new Employee("NITISH",102,25000,"PROGRAMMER",services.findScheme(25000,"PROGRAMMER"));
+		Employee employ3 = new Employee("SANDEEP",103,21000,"PROGRAMMER",services.findScheme(21000,"PROGRAMMER"));
+		Employee employ4 = new Employee("ANUP",104,21000,"PROGRAMMER",services.findScheme(21000,"PROGRAMMER"));
 		employ1.list.put(101,employ1);
 		employ2.list.put(102, employ2);
 		employ3.list.put(103, employ3);
+		employ4.list.put(104, employ4);
 		System.out.println("Enter the Name : ");
 		String name = sc.next();
 		System.out.println("Enter the Id of employee : ");
@@ -31,10 +32,9 @@ public class MainClass
 		String scheme = services.findScheme(salary,designation);
 		Employee employee = new Employee(name,id,salary,designation,scheme);
 		employee.list.put(id, employee);
-		System.out.println("**********Employ added successfully**********");
+		System.out.println("**********EmployEE added successfully.**********");
 		System.out.println(employee);
 		employee.list.remove(101);
-		System.out.println("Employee is removed now");
+		System.out.println("Employee is removed now.");
 	}
-
 }
